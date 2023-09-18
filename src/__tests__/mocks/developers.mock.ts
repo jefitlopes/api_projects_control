a1ce3edb13a2477eaec8fbdc26ee07d72533a0c3
jefitlopes <jefitlopes@gmail.com>
2023-09-18 18:09:02 -0300

    fix: fixed tests

--- src/__tests__/mocks/developers.mock.ts
+++ src/__tests__/mocks/developers.mock.ts
@@ -1,40 +1,40 @@
 const developer1 = {
-    email: 'ugo@kenzie.com.br',
-    name: 'Ugo',
-}
+	email: "joao@mail.com.br",
+	name: "Joao",
+};
 
 const developer2 = {
-    email: 'cauan@kenzie.com.br',
-    name: 'Cauan',
-}
+	email: "cauan@mail.com.br",
+	name: "Cauan",
+};
 
 const developer3 = {
-    email: 'andyara@kenzie.com.br',
-    name: 'Andyara',
-}
+	email: "andyara@mail.com.br",
+	name: "Andyara",
+};
 
 const developer4 = {
-    email: 'lucas@kenzie.com.br',
-    name: 'Lucas',
-}
+	email: "lucas@mail.com.br",
+	name: "Lucas",
+};
 
 const developerUpdate1 = {
-    email: 'ugo.roveda@kenzie.com.br',
-}
+	email: "joao.jr@mail.com.br",
+};
 
 const developerUpdate2 = {
-    email: 'cauan.franca@kenzie.com.br',
-}
+	email: "cauan.franca@mail.com.br",
+};
 const developerUpdate3 = {
-    email: 'andy@kenzie.com.br',
-}
+	email: "andy@mail.com.br",
+};
 
 export {
-    developer1,
-    developer2,
-    developer3,
-    developer4,
-    developerUpdate1,
-    developerUpdate2,
-    developerUpdate3,
-}
+	developer1,
+	developer2,
+	developer3,
+	developer4,
+	developerUpdate1,
+	developerUpdate2,
+	developerUpdate3,
+};
